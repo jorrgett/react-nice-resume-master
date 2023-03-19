a792576572e266a63998cbc41a82f81c7a412ef5
jorrgett <jorgerafaeltorrestoro@gmail.com>
2023-03-19 15:54:47 -0400

    first commit

---
+++ src/Components/About.js
@@ -0,0 +1,71 @@
+import React, { Component } from "react";
+import Fade from "react-reveal";
+
+class About extends Component {
+  render() {
+    if (!this.props.data) return null;
+
+    const name = this.props.data.name;
+    const profilepic = "images/" + this.props.data.image;
+    const bio = this.props.data.bio;
+    const bio2 = this.props.data.bio2;
+    const bio3 = this.props.data.bio3;
+    const bio4 = this.props.data.bio4;
+    const street = this.props.data.address.street;
+    const city = this.props.data.address.city;
+    const state = this.props.data.address.state;
+    const zip = this.props.data.address.zip;
+    const phone = this.props.data.phone;
+    const email = this.props.data.email;
+    const resumeDownload = this.props.data.resumedownload;
+
+    return (
+      <section id="about">
+        <Fade duration={1000}>
+          <div className="row">
+            <div className="three columns">
+              <img
+                className="profile-pic"
+                src={profilepic}
+                alt="Nordic Giant Profile Pic"
+              />
+            </div>
+            <div className="nine columns main-col">
+              <h2>Acerca de Mí</h2>
+
+              <p>{bio}</p>
+              <p>{bio2}</p>
+              <p>{bio3}</p>
+              <p>{bio4}</p>
+              <div className="row">
+                <div className="columns contact-details">
+                  <h2>Contacto</h2>
+                  <p className="address">
+                    <span>
+                      {street}
+                      <br />
+                      {city} {state}, {zip}
+                    </span>
+                    <br />
+                    <span>{phone}</span>
+                    <br />
+                    <span>{email}</span>
+                  </p>
+                </div>
+                <div className="columns download">
+                  <p>
+                    <a href={resumeDownload} className="button">
+                      <i className="fa fa-download"></i>Descargar CV
+                    </a>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </Fade>
+      </section>
+    );
+  }
+}
+
+export default About;
---
+++ src/Components/Resume.js
@@ -0,0 +1,117 @@
+import React, { Component } from "react";
+import Slide from "react-reveal";
+
+class Resume extends Component {
+  getRandomColor() {
+    const allowedColors = ["432C7A", "80489C", "FF8FB1", "FCE2DB"];
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+      color += allowedColors[
+        Math.floor(Math.random() * allowedColors.length)
+      ].substring(i, i + 1);
+    }
+    return color;
+  }
+
+  render() {
+    if (!this.props.data) return null;
+
+    const skillmessage = this.props.data.skillmessage;
+    const education = this.props.data.education.map(function (education) {
+      return (
+        <div key={education.school}>
+          <h3>{education.school}</h3>
+          <p className="info">
+            {education.degree} <span>&bull;</span>
+            <em className="date">{education.graduated}</em>
+          </p>
+          <p>{education.description}</p>
+        </div>
+      );
+    });
+
+    const work = this.props.data.work.map(function (work) {
+      return (
+        <div key={work.company}>
+          <h3>{work.company}</h3>
+          <p className="info">
+            {work.title}
+            <span>&bull;</span> <em className="date">{work.years}</em>
+          </p>
+          <p>
+            <ul>
+              <li>{work.description1}</li>
+              <li>{work.description2}</li>
+              <li>{work.description3}</li>
+              <li>{work.description4}</li>
+              <li>{work.description5}</li>
+            </ul>
+          </p>
+        </div>
+      );
+    });
+
+    const skills = this.props.data.skills.map((skills) => {
+      const backgroundColor = this.getRandomColor();
+      const className = "bar-expand " + skills.name.toLowerCase();
+      const width = skills.level;
+
+      return (
+        <li key={skills.name}>
+          <span style={{ width, backgroundColor }} className={className}></span>
+          <em>{skills.name}</em>
+        </li>
+      );
+    });
+
+    return (
+      <section id="resume">
+        <Slide left duration={1300}>
+          <div className="row education">
+            <div className="three columns header-col">
+              <h1>
+                <span>Educación</span>
+              </h1>
+            </div>
+
+            <div className="nine columns main-col">
+              <div className="row item">
+                <div className="twelve columns">{education}</div>
+              </div>
+            </div>
+          </div>
+        </Slide>
+
+        <Slide left duration={1300}>
+          <div className="row work">
+            <div className="three columns header-col">
+              <h1>
+                <span>Experiencia</span>
+              </h1>
+            </div>
+
+            <div className="nine columns main-col">{work}</div>
+          </div>
+        </Slide>
+
+        <Slide left duration={1300}>
+          <div className="row skill">
+            <div className="three columns header-col">
+              <h1>
+                <span>Habilidades</span>
+              </h1>
+            </div>
+
+            <div className="nine columns main-col">
+              <div className="bars">
+                <ul className="skills">{skills}</ul>
+              </div>
+            </div>
+          </div>
+        </Slide>
+      </section>
+    );
+  }
+}
+
+export default Resume;
